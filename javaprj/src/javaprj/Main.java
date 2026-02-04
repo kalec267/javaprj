f1cdcffd55686f9361aed161b43916532ed46410
kalec267 <kalec267@gmail.com>
2026-02-04 12:16:01 +0900

    개발11

--- javaprj/src/javaprj/Main.java
+++ javaprj/src/javaprj/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("dev1 개발코드11");
-		System.out.println("dev1 개발코드11");
-		System.out.println("dev1 개발코드11");
 
 	}
 
