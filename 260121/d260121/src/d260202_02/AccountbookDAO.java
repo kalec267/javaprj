04cccbd6436915ca2ee2ff12c9dad7ca5e4663be
kalec267 <kalec267@gmail.com>
2026-02-04 12:37:01 +0900

    260204
    accountbook v1.1

--- 260121/d260121/src/d260202_02/AccountbookDAO.java
+++
@@ -1,7 +0,0 @@
-package d260202_02;
-
-import java.util.List;
-
-public interface AccountbookDAO {
-	public Accountbook findByCategory(String category);
-}
--- 260121/d260121/src/d260202_02/Test.java
+++
@@ -1,9 +0,0 @@
-package d260202_02;
-
-public class Test {
-
-	public static void main(String[] args) {
-
-
-	}
-}
